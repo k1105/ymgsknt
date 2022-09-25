2f1a0ca658293363ce8186ebc5e16c0866d687ce
k1105 <kanapon1105@yahoo.co.jp>
2022-09-26 01:18:15 +0900

    header styleの修正

--- components/header.tsx
+++ components/header.tsx
@@ -4,15 +4,44 @@ export const siteTitle = "Next.js Sample Website";
 
 export default function Header() {
   return (
-    <header>
-      <p>KANATA YAMAGISHI (((UnderConstruction)))</p>
+    <header style={{ marginBottom: "50px" }}>
+      <div className="header-container">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "90vw",
+            margin: "0 auto",
+          }}
+        >
+          <p>KANATA YAMAGISHI (((UnderConstruction)))</p>
 
-      <Link href="/">
-        <a>Database, </a>
-      </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+          <div>
+            <Link href="/">
+              <p>Database</p>
+            </Link>
+            <Link href="/about">
+              <p>About</p>
+            </Link>
+          </div>
+        </div>
+        <hr style={{ margin: "0" }} />
+      </div>
+      <style jsx>{`
+        .header-container {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100vw;
+          z-index: 99;
+          background: white;
+        }
+        @media (prefers-color-scheme: dark) {
+          .header-container {
+            background: black;
+          }
+        }
+      `}</style>
     </header>
   );
 }
